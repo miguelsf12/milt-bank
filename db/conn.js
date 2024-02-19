24676f1e2c1b4ec1ca674471e0dae36c93c6307f
Miguel <miguelsantosf12@gmail.com>
2024-02-19 16:09:04 -0300

    Alterado de railway para local

--- db/conn.js
+++ db/conn.js
@@ -1,26 +1,26 @@
 const { Sequelize } = require('sequelize')
 
 // RAILWAY
-const sequelize = new Sequelize({
-  username: process.env.DB_USERNAME,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DATABASE,
-  dialect: 'mysql',
-  port: process.env.DB_PORT,
-  host: process.env.DB_HOST,
-})
+// const sequelize = new Sequelize({
+//   username: process.env.DB_USERNAME,
+//   password: process.env.DB_PASSWORD,
+//   database: process.env.DATABASE,
+//   dialect: 'mysql',
+//   port: process.env.DB_PORT,
+//   host: process.env.DB_HOST,
+// })
 
 // LOCALHOST
-// const sequelize = new Sequelize('miltbank', 'root', '', {
-//   host: 'localhost',
-//   dialect: 'mysql'
-// })
+const sequelize = new Sequelize('miltbank', 'root', '', {
+  host: 'localhost',
+  dialect: 'mysql'
+})
 // LOCALHOST
-// try {
-//   sequelize.authenticate()
-//   console.log('Conectado com sucesso!')
-// } catch (err) {
-//   console.log(`Houve um erro: ${err}`)
-// }
+try {
+  sequelize.authenticate()
+  console.log('Conectado com sucesso!')
+} catch (err) {
+  console.log(`Houve um erro: ${err}`)
+}
 
 module.exports = sequelize
